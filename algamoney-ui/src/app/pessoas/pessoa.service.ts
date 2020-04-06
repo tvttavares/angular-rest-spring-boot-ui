77a9f9967b92517eb7b1367995ce8c4bf1aa1922
thiago <tvttavares@gmail.com>
2020-04-06 15:43:29 -0400

    Desafio: implementando a mudança de status de pessoas

--- algamoney-ui/src/app/pessoas/pessoa.service.ts
+++ algamoney-ui/src/app/pessoas/pessoa.service.ts
@@ -52,4 +52,13 @@ export class PessoaService {
     return this.http.delete(`${this.pessoasUrl}/${codigo}` , { headers })
     .toPromise();
   }
+
+  mudarStatus(codigo: number, ativo: boolean): Promise<void> {
+    const headers = new HttpHeaders().append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==')
+      .append('Content-Type', 'application/json');
+
+    return this.http.put(`${this.pessoasUrl}/${codigo}/ativo`, ativo, { headers })
+      .toPromise()
+      .then(() => null);
+  }
 }
