7143776e0365cc5c31991973cc6b6829dcb1cc0c
thiago <tvttavares@gmail.com>
2020-04-08 17:14:38 -0400

    Exibindo o nome do usuario logado

--- 19.8-exibindo-o-nome-do-usuario-logado/src/app/core/navbar/navbar.component.ts
+++ 19.8-exibindo-o-nome-do-usuario-logado/src/app/core/navbar/navbar.component.ts
@@ -1,16 +1,16 @@
+import { AuthService } from './../../seguranca/auth.service';
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/seguranca/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
-  constructor(
-    public auth: AuthService
-  ) {}
+  constructor(private auth: AuthService) { }
 
-  exibindoMenu = false;
+  ngOnInit() {
+  }
+
 }
