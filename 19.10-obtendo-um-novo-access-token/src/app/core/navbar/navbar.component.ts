5e5456db8097daef068e7116acdff296c0636afb
thiago <tvttavares@gmail.com>
2020-04-08 20:55:39 -0400

    Obtendo um novo access token

--- 19.10-obtendo-um-novo-access-token/src/app/core/navbar/navbar.component.ts
+++ 19.10-obtendo-um-novo-access-token/src/app/core/navbar/navbar.component.ts
@@ -1,20 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/seguranca/auth.service';
+
+import { AuthService } from './../../seguranca/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
-  constructor(
-    public auth: AuthService
-  ) {}
+  constructor(private auth: AuthService) { }
 
-  exibindoMenu = false;
+  ngOnInit() {
+  }
 
   criarNovoAccessToken() {
     this.auth.obterNovoAccessToken();
   }
+
 }
